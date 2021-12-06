c672104d0910332d5ea3f784de300670057e8d3c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-12-06 18:20:13 +0100

    Fix rotation animation

--- src/main/java/com/dremoline/portablemobs/PortableMobItemStackRenderer.java
+++ src/main/java/com/dremoline/portablemobs/PortableMobItemStackRenderer.java
@@ -21,40 +21,40 @@ import java.util.Optional;
 
 public class PortableMobItemStackRenderer extends ItemStackTileEntityRenderer {
 
+    public static final int ROTATION_TIME = 5000;
+
     public static final PortableMobItemStackRenderer INSTANCE = new PortableMobItemStackRenderer();
 
-    public static PortableMobItemStackRenderer getInstance() {
+    public static PortableMobItemStackRenderer getInstance(){
         return INSTANCE;
     }
 
     @Override
-    public void renderByItem(ItemStack itemStack, ItemCameraTransforms.TransformType transformType, MatrixStack matrixStack, IRenderTypeBuffer typeBuffer, int combinedLight, int combinedOverlay) {
+    public void renderByItem(ItemStack itemStack, ItemCameraTransforms.TransformType transformType, MatrixStack matrixStack, IRenderTypeBuffer typeBuffer, int combinedLight, int combinedOverlay){
         IBakedModel model = ClientUtils.getItemRenderer().getItemModelShaper().getItemModel(itemStack);
         renderDefaultItem(itemStack, matrixStack, transformType, typeBuffer, combinedLight, combinedOverlay, model);
 
-        if (!itemStack.hasTag() || !itemStack.getTag().getBoolean("has_entity")) {
+        if(!itemStack.hasTag() || !itemStack.getTag().getBoolean("has_entity")){
             return;
         }
 
         CompoundNBT compound = itemStack.getTag();
         Optional<EntityType<?>> optional = EntityType.byString(compound.getString("entity_type"));
-        if (optional.isPresent()) {
+        if(optional.isPresent()){
             Entity living = optional.get().create(ClientUtils.getWorld());
-            if (living != null) {
+            if(living != null){
                 living.load(compound.getCompound("entity_data"));
 
                 matrixStack.pushPose();
+                matrixStack.translate(0.5, 0, 0.5);
+                matrixStack.mulPose(new Quaternion(0, (System.currentTimeMillis() % ROTATION_TIME) / (float)ROTATION_TIME * 360, 0, true));
 
                 float width = living.getBbWidth();
                 float height = living.getBbHeight();
                 float size = 0.9f;
-                float length = size / (float) Math.sqrt(2 * width * width + height * height);
-                matrixStack.scale(length, length, length);
-
-                matrixStack.translate(0.5, 0.5, 0.5);
-                matrixStack.mulPose(new Quaternion(0, (System.currentTimeMillis() % 1000) / 1000f * 360, 0, true));
-                System.out.println("rotation: "+((System.currentTimeMillis() %1000) /1000f * 360));
-                matrixStack.translate(size / 2, size / 2, size / 2);
+                float length = (float)Math.sqrt(2 * width * width + height * height) * 1.2f;
+                float scalar = size / length;
+                matrixStack.scale(scalar, scalar, scalar);
 
                 renderEntity(living, matrixStack, typeBuffer, combinedLight);
 
@@ -64,19 +64,19 @@ public class PortableMobItemStackRenderer extends ItemStackTileEntityRenderer {
 
     }
 
-    private static <T extends Entity> void renderEntity(T living, MatrixStack matrixStack, IRenderTypeBuffer typeBuffer, int combinedLight) {
+    private static <T extends Entity> void renderEntity(T living, MatrixStack matrixStack, IRenderTypeBuffer typeBuffer, int combinedLight){
         EntityRenderer<? super T> renderer = ClientUtils.getMinecraft().getEntityRenderDispatcher().getRenderer(living);
         renderer.render(living, 0, 0, matrixStack, typeBuffer, combinedLight);
     }
 
-    private static void renderDefaultItem(ItemStack itemStack, MatrixStack matrixStack, ItemCameraTransforms.TransformType cameraTransforms, IRenderTypeBuffer renderTypeBuffer, int combinedLight, int combinedOverlay, IBakedModel model) {
+    private static void renderDefaultItem(ItemStack itemStack, MatrixStack matrixStack, ItemCameraTransforms.TransformType cameraTransforms, IRenderTypeBuffer renderTypeBuffer, int combinedLight, int combinedOverlay, IBakedModel model){
         ItemRenderer renderer = ClientUtils.getMinecraft().getItemRenderer();
 
         matrixStack.pushPose();
 
-        if (model.isLayered()) {
+        if(model.isLayered()){
             net.minecraftforge.client.ForgeHooksClient.drawItemLayered(renderer, model, itemStack, matrixStack, renderTypeBuffer, combinedLight, combinedOverlay, true);
-        } else {
+        }else{
             RenderType rendertype = RenderTypeLookup.getRenderType(itemStack, true);
             IVertexBuilder ivertexbuilder;
 
