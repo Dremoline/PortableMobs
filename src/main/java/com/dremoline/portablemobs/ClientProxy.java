5eb8ea5781108e18772ecd8d95cad74822e35113
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-07-17 13:50:50 +0200

    Update to Forge 41.0.94 #2

--- src/main/java/com/dremoline/portablemobs/ClientProxy.java
+++ src/main/java/com/dremoline/portablemobs/ClientProxy.java
@@ -4,7 +4,7 @@ import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ModelBakeEvent;
+import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -12,12 +12,12 @@ import net.minecraftforge.fml.common.Mod;
 public class ClientProxy {
 
     @SubscribeEvent
-    public static void onModelBake(ModelBakeEvent e) {
-        for (PortableMobTypes type : PortableMobTypes.values()) {
+    public static void onModelBake(ModelEvent.BakingCompleted e){
+        for(PortableMobTypes type : PortableMobTypes.values()){
             ResourceLocation location = new ModelResourceLocation("portablemobs:" + type.toSuffix() + "_capture_cell", "inventory");
-            BakedModel model = e.getModelRegistry().get(location);
-            if (model != null) {
-                e.getModelRegistry().put(location, new PortableMobBakedItemModel(model));
+            BakedModel model = e.getModels().get(location);
+            if(model != null){
+                e.getModels().put(location, new PortableMobBakedItemModel(model));
             }
         }
     }
