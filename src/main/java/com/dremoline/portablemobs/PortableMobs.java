2872e86e116d772fc1868366933d474105ff815d
Dremoline <steviesm@ziggo.nl>
2023-07-12 19:42:28 +0200

    -Added advancements -Shift-Right click fixed for entities with interaction.

--- src/main/java/com/dremoline/portablemobs/PortableMobs.java
+++ src/main/java/com/dremoline/portablemobs/PortableMobs.java
@@ -1,13 +1,13 @@
 package com.dremoline.portablemobs;
 
-import com.dremoline.portablemobs.generators.PortableMobsLanguageGenerator;
-import com.dremoline.portablemobs.generators.PortableMobsModelGenerator;
-import com.dremoline.portablemobs.generators.PortableMobsRecipeGenerator;
-import com.dremoline.portablemobs.generators.PortableMobsTagGenerator;
+import com.dremoline.portablemobs.generators.*;
 import com.supermartijn642.core.item.CreativeItemGroup;
 import com.supermartijn642.core.registry.GeneratorRegistrationHandler;
 import com.supermartijn642.core.registry.RegistrationHandler;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.advancements.critereon.PlayerTrigger;
+import net.minecraft.resources.ResourceLocation;
 
 /**
  * Created 7/7/2020 by SuperMartijn642
@@ -15,6 +15,8 @@ import net.fabricmc.api.ModInitializer;
 public class PortableMobs implements ModInitializer {
 
     public static final CreativeItemGroup GROUP = CreativeItemGroup.create("portablemobs", PortableMobTypes.BASIC::getItem);
+
+    public static PlayerTrigger playerCaptureTrigger, captureTrigger;
 
     @Override
     public void onInitialize() {
@@ -29,5 +31,9 @@ public class PortableMobs implements ModInitializer {
         generatorHandler.addGenerator(PortableMobsTagGenerator::new);
         generatorHandler.addGenerator(PortableMobsRecipeGenerator::new);
         generatorHandler.addGenerator(PortableMobsModelGenerator::new);
+        generatorHandler.addGenerator(PortableMobsAdvancementGenerator::new);
+
+        playerCaptureTrigger = CriteriaTriggers.register(new PlayerTrigger(new ResourceLocation("portablemobs", "playercapture")));
+        captureTrigger = CriteriaTriggers.register(new PlayerTrigger(new ResourceLocation("portablemobs", "capture")));
     }
 }
