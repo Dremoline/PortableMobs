2404b192b86b0820f3975cd4f053868d747e860c
Dremoline <steviesm@ziggo.nl>
2024-09-12 16:45:48 +0200

    -Custom item renderer not registering, fixed.

--- src/main/java/com/dremoline/portablemobs/PortableMobs.java
+++ src/main/java/com/dremoline/portablemobs/PortableMobs.java
@@ -1,6 +1,7 @@
 package com.dremoline.portablemobs;
 
 import com.dremoline.portablemobs.generators.*;
+import com.supermartijn642.core.CommonUtils;
 import com.supermartijn642.core.item.CreativeItemGroup;
 import com.supermartijn642.core.registry.GeneratorRegistrationHandler;
 import com.supermartijn642.core.registry.RegistrationHandler;
@@ -29,6 +30,9 @@ public class PortableMobs {
             handler.registerItemCallback(type::registerItem);
         }
         handler.registerRecipeSerializer("upgrade_capture_cell", PortableMobUpgradeRecipe.SERIALIZER);
+
+        if(CommonUtils.getEnvironmentSide().isClient())
+            PortableMobsClient.register();
 
         GeneratorRegistrationHandler generatorHandler = GeneratorRegistrationHandler.get("portablemobs");
         generatorHandler.addGenerator(PortableMobsLanguageGenerator::new);
